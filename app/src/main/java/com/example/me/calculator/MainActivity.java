280d40d2a9730037ddc59d8526533ab1d2657fd4
SeriousTingz <Lakhanpar.Abdul@gmail.com>
2017-08-13 04:37:36 +0100

    Added Buttons, TextViews and declared variables

--- app/src/main/java/com/example/me/calculator/MainActivity.java
+++ app/src/main/java/com/example/me/calculator/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.me.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    EditText number1, number2;
+    Button add, subtract, divide, multiply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
